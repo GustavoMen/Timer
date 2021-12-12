f7bd2d11407ca0a43f31470a7d90add0d8aa382a
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2021-12-11 21:03:18 -0300

    Timer

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var numero = 0;
+var interval;
+
+
+function atualizarTexto() {
+    let h = document.getElementsByTagName("h1")[0]
+    h.innerHTML += " " + numero;
+    numero++;
+}
+
+function iniciar() {
+   interval = setInterval (atualizarTexto , 100)
+}
+
+function parar() {
+    clearInterval(interval)
+}
+
+function limpar() {
+    clearTimeout(interval)
+}
