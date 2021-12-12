1b969ab3a093a07d4b3e41b174e3d702b7efbe5c
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2021-12-11 23:21:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-var numero = 0;
-var interval;
+var segundo = 0;
+var minuto = 0;
+var hora = 0;
+var timer;
 
 
 function atualizarTexto() {
     let h = document.getElementsByTagName("h1")[0]
-    h.innerHTML += " " + numero;
-    numero++;
+    h.innerHTML = minuto + " : " + segundo;
+    if(segundo > 59){
+        minuto++;
+        segundo = 0 
+    }else{ 
+    h.innerHTML = minuto + " : " + segundo;
+    segundo++
+    }
 }
 
 function iniciar() {
-   interval = setInterval (atualizarTexto , 100)
+   timer = setInterval (atualizarTexto , 100)
 }
 
 function parar() {
-    clearInterval(interval)
+    clearInterval(timer)
 }
 
 function limpar() {
-    clearTimeout(interval)
+    clearTimeout(timer)
 }
